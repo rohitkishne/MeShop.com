727b9b11cd7a3f3d5d799870aafb783d94e39b9a
Rohit kishne <rohitkishne3@gmail.com>
2023-08-03 00:27:01 +0530

    Improved Filter feature

--- shop/priceRangefilter.js
+++ shop/priceRangefilter.js
@@ -2,43 +2,30 @@
 
 setTimeout(() => {
     const priceRangeFilter = document.getElementById('price-range');
+    const ratingFilter = document.getElementById('range')
     const applyFilter = document.getElementById('apply-to-filter');
 
+    //Price Range filter ----------------------------------->
     let priceofItems = [];
-
+    
     priceRangeFilter.addEventListener('change', (event) => {
         try {
             const productId = event.target.id;
             const range = productId.split('-');
-            mensItem.innerHTML = ``;
-            womensItem.innerHTML = ``;  
+
             if(event.target.checked)
             { 
                 priceofItems.push(parseInt(range[0]))
                 priceofItems.push(parseInt(range[1]))
-                const filterProductbyPrice = searchPriceRange(findTwoValue(priceofItems), data);
-                renderItems(filterProductbyPrice)
             }
-            else if(priceofItems.length !== 0){
+            else if(priceofItems.length !== 0)
+            {
                 //take the index of first range price value and delete it from the priceofitem array
                 let range1pos = priceofItems.indexOf(parseInt(range[0]))
                 priceofItems.splice(range1pos,1)
                 //take the index of second range price value and delete it from the priceofitem array
                 let range2pos = priceofItems.indexOf(parseInt(range[1]))
-                priceofItems.splice(range2pos,1)
-
-                if(priceofItems.length === 0)
-                {
-                    renderItems(data);
-                }
-                else{
-                    const filterProductbyPrice = searchPriceRange(findTwoValue(priceofItems), data);
-                    renderItems(filterProductbyPrice)
-                }
-                
-            }
-            else{
-                renderItems(data);
+                priceofItems.splice(range2pos,1)   
             }
     
         } catch (error) {
@@ -46,6 +33,7 @@ setTimeout(() => {
         }
     })
 
+    //It is finding min price and max price from price range in price Range array
     function findTwoValue(priceArr) {
         let twoPrice = [];
         let lowPrice = 1000000000;
@@ -68,11 +56,72 @@ setTimeout(() => {
     }
 
 
+    // Rating filter --------------------------------->
+    let rating = 0;
+    ratingFilter.addEventListener('change', (event) => {
+        try {
+            rating = parseInt(event.target.value);
+        } catch (error) {
+            console.log('Error: ', error)
+        }
+    })
+
+    //whenever i click on the apply filter button, each and every filter will apply to shop and shows a result
+    //if we dont apply a button, by default result will be display as usual
+
+    applyFilter.addEventListener('click', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+
+        mensItem.innerHTML = ``;
+        womensItem.innerHTML = ``;
+
+        if(priceofItems.length === 0 && rating ===0)
+        {
+            console.log('first')
+            renderItems(data);
+        }
+        else if(priceofItems.length !==0 && rating === 0)
+        {
+            console.log('second')
+            const filterProduct = searchPriceRange(findTwoValue(priceofItems),data);
+            renderItems(filterProduct)
+        }
+        else if(priceofItems.length === 0 && rating !== 0)
+        {
+            console.log('third')
+            const filterProduct = searchProductByRating(rating, data);
+            renderItems(filterProduct)
+        }
+        else{
+            console.log('fourth')
+            const filterProduct = search(findTwoValue(priceofItems),rating, data);
+            renderItems(filterProduct)
+        }
+        
+    })  
+
+    //Search and filter the product based on price range only
     function searchPriceRange(range, products) {
         return products.filter((product) => {
             return product.price >=range[0] && product.price<=range[1];
         })
     }
+
+    //Search and filter the product based on ratings only
+    function searchProductByRating(rating, products) {
+        return products.filter((product) => {
+            return product.rating.rate >= rating;
+        })
+    }
+
+    //Search and filter the product based on both price range and ratings
+    function search(range, rating, products) {
+        return products.filter((product) => {
+            return (product.price >=range[0] && product.price<=range[1]) && product.rating.rate >=rating;
+        })
+    }
+
 }, 1000);
 
 
